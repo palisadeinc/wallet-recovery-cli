8138d61f1a0d0b6fd63f2ba9fde5e64103f6e2aa
Manthan Dave <manthanhd@live.com>
2025-06-03 12:26:34 +0100

    fix: test

--- utils/encryption_test.go
+++ utils/encryption_test.go
@@ -9,22 +9,15 @@ import (
 func TestEncryptDecrypt(t *testing.T) {
 	// Test encrypting and decrypting a message
 	message := "Hello, world!"
-	passwordBytes := []byte("password1234")
-	ciphertext, err := utils.EncryptData(passwordBytes, []byte(message))
+	ciphertext, err := utils.EncryptData([]byte("password1234"), []byte(message))
 	if err != nil {
 		t.Errorf("encrypt failed: %v", err)
 	}
-	plaintext, err := utils.DecryptData(passwordBytes, ciphertext)
+	plaintext, err := utils.DecryptData([]byte("password1234"), ciphertext)
 	if err != nil {
 		t.Errorf("decrypt failed: %v", err)
 	}
 	if string(plaintext) != message {
 		t.Errorf("decrypted message does not match original")
-	}
-}
-
-func clear(b []byte) {
-	for i := range b {
-		b[i] = 0
 	}
 }
