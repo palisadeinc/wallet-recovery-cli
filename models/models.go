a4954c47221d62cf0fc002b5b8b343dc34716eb1
Manthan Dave <manthanhd@live.com>
2025-09-22 21:53:09 +0100

    chore: add mpc algorithm

--- models/models.go
+++ models/models.go
@@ -7,10 +7,16 @@ const (
 	KeyAlgorithmED25519   KeyAlgorithm = "ED25519"
 )
 
+const (
+	MPCAlgorithmDKLS19  string = "dkls19"
+	MPCAlgorithmPDKLS23 string = "pdkls23"
+)
+
 type RecoveryDataObject struct {
 	Index                         uint   `json:"index"`
 	RecoveryPublicKeyHex          string `json:"recoveryPublicKeyHex"`
 	PartialRecoveryDataBase64     string `json:"partialRecoveryDataHex"`
 	WalletRootPublicKeyPkixBase64 string `json:"walletRootPublicKeyPkixBase64"`
 	WalletPublicKeyBase64         string `json:"walletPublicKeyBase64"`
+	MpcAlgorithm                  string `json:"mpcAlgorithm"`
 }
